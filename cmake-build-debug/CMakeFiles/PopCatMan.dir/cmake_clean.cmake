302211f8e31493ca2698211b34ba709c52cee5a5
Hans Podzun <hvp@sfu.ca>
2021-01-10 19:18:30 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PopCatMan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PopCatMan.dir/main.cpp.obj"
+  "PopCatMan.exe"
+  "PopCatMan.exe.manifest"
+  "PopCatMan.pdb"
+  "libPopCatMan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PopCatMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
